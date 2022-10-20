1de08a987e2dbf056421c5d252fc1215a45acabd
Robin Christian Escobia <116277247+Robin0212@users.noreply.github.com>
2022-10-20 20:15:55 +0800

    Add files via upload

---
+++ BloodData(4).java
@@ -0,0 +1,17 @@
+class BloodData{
+    private String bloodType;
+    private String rhFactor;  
+    
+	public void setBloodType(String bdt){
+		bloodType = bdt;
+	}
+	public String getBloodType(){
+		return bloodType;	
+	}
+	public void setRhFactor(String rhf){
+		rhFactor = rhf;
+	}
+	public String getRhFactor(){
+		return rhFactor;
+	}
+}
